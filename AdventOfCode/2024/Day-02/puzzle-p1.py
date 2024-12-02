61571a45155100b25ae82b60f706a4c40d5b0086
Mark Prill <mark.prill@gmail.com>
2024-12-02 12:07:07 -0500

    Interim commit.

--- AdventOfCode/2024/Day-02/puzzle-p1.py
+++ AdventOfCode/2024/Day-02/puzzle-p1.py
@@ -18,18 +18,15 @@ def main():
     with open(filename, 'r', encoding='utf-8') as f:
         input_data = [[int(x) for x in line.split(' ')] for line in f.read().splitlines()]
 
-    unsafe = 0
+    total_safe = 0
     for line in input_data:
         dist_count = Counter([pair[1]-pair[0] for pair in pairwise(line)])
         sign_count = Counter([(x > 0) - (x < 0) for x in dist_count])
 
-        if len(sign_count) != 1:
-            unsafe += 1
-        else:
-            if any([c not in [-3, -2, -1, 1, 2, 3] for c in dist_count]):
-                unsafe += 1
+        if len(sign_count) == 1 and all([c in [-3, -2, -1, 1, 2, 3] for c in dist_count]):
+            total_safe += 1
     
-    print(f'Number safe: {len(input_data)-unsafe}')
+    print(f'Number safe: {total_safe}')
 
 
 if __name__ == '__main__':
--- AdventOfCode/2024/Day-02/puzzle-p2.py
+++ AdventOfCode/2024/Day-02/puzzle-p2.py
@@ -10,17 +10,14 @@ def pairwise(iterable):
     next(b)
     return [p for p in zip(a, b)]
 
-def is_unsafe(numlist):
+def is_safe(numlist):
     dist_count = Counter([pair[1]-pair[0] for pair in pairwise(numlist)])
     sign_count = Counter([(x > 0) - (x < 0) for x in dist_count])
 
-    unsafe = False
-    if len(sign_count) != 1:
-        unsafe = True
-    else:
-        if any([c not in [-3, -2, -1, 1, 2, 3] for c in dist_count]):
-           unsafe = True
-    return unsafe
+    if len(sign_count) == 1 and all([c in [-3, -2, -1, 1, 2, 3] for c in dist_count]):
+        return True
+    return False
+
 
 def main():
     filename = fr'./AdventOfCode/2024/Day-{DAY}/input-example.txt'
@@ -29,16 +26,17 @@ def main():
     with open(filename, 'r', encoding='utf-8') as f:
         input_data = [[int(x) for x in line.split(' ')] for line in f.read().splitlines()]
 
-    unsafe = 0
+    total_safe = 0
     for line in input_data:
-        if is_unsafe(line):
+        if is_safe(line):
+            total_safe += 1
+        else:
             for line_m1 in combinations(line, len(line)-1):
-                if not is_unsafe(line_m1):
+                if is_safe(line_m1):
+                    total_safe += 1
                     break
-            else:
-                unsafe += 1
     
-    print(f'Number safe: {len(input_data)-unsafe}')
+    print(f'Number safe: {total_safe}')
 
 
 if __name__ == '__main__':
