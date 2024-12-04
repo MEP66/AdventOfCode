320a016705c47ffe583e2195b6d1b388a5c914fd
Mark Prill <mark.prill@gmail.com>
2024-12-04 08:44:01 -0500

    Cleanup

--- AdventOfCode/2024/Day-04/puzzle-p1.py
+++ AdventOfCode/2024/Day-04/puzzle-p1.py
@@ -1,17 +1,16 @@
 from collections import Counter
 
-
 DAY = '04'
 
 #Coordinates are in (x, y) or (c, r)
 DIR = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
 
 def get_letters(scoord, dir, cnt, lmap):
-    ncoord = (scoord[0] + dir[0], scoord[1] + dir[1])
-    if 0 <= ncoord[0] < len(lmap) and 0 <= ncoord[1] < len(lmap[0]):
-        letter = lmap[ncoord[1]][ncoord[0]]
+    if 0 <= scoord[0] < len(lmap) and 0 <= scoord[1] < len(lmap[0]):
+        letter = lmap[scoord[1]][scoord[0]]
         cnt -= 1
         if cnt > 0:
+            ncoord = (scoord[0] + dir[0], scoord[1] + dir[1])
             if next_letter := get_letters(ncoord, dir, cnt, lmap):
                 return letter + next_letter
             else:
@@ -21,13 +20,14 @@ def get_letters(scoord, dir, cnt, lmap):
     else:
         return None
 
-def get_words(coord, puzmap):
+def num_xmas(coord, puzmap):
     ci, ri = coord
-    words = list()
+    xmas_count = 0
     for d in DIR:
-        if new_word := get_letters(coord, d, 3, puzmap):
-            words.append(puzmap[ri][ci] + new_word)
-    return words
+        if new_word := get_letters(coord, d, 4, puzmap):
+            if new_word == 'XMAS':
+                xmas_count += 1
+    return xmas_count
 
 def main():
     filename = fr'./AdventOfCode/2024/Day-{DAY}/input-example.txt'
@@ -36,13 +36,13 @@ def main():
     with open(filename, 'r', encoding='utf-8') as f:
         puzzle = [[c for c in line] for line in f.read().splitlines()]
 
-    word_count = Counter()
+    xmas_count = 0
 
     for ri, row in enumerate(puzzle):
         for ci, char in enumerate(row):
             if char == 'X':
-                word_count.update(get_words((ci, ri), puzzle))
-    print(f'Total XMAS count: {word_count["XMAS"]}')
+                xmas_count += num_xmas((ci, ri), puzzle)
+    print(f'Total XMAS count: {xmas_count}')
     
 
 if __name__ == '__main__':
