f05afb2beccee3e5b8700d889b489800232994d0
Mark Prill <mark.prill@gmail.com>
2024-12-05 04:50:10 -0500

    Interim commit.

---
+++ AdventOfCode/2024/Day-05/puzzle-p1.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+
+DAY = '05'
+
+
+def file_line_gen(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            yield line.strip()
+
+
+def main():
+    filename = fr'./AdventOfCode/2024/Day-{DAY}/input-example.txt'
+    filename = fr'./AdventOfCode/2024/Day-{DAY}/input.txt'
+    
+    before = defaultdict(set)
+    after = defaultdict(set)
+
+    get_line = file_line_gen(filename) 
+    line = next(get_line)
+    while line != '':
+        params = [int(x) for x in line.split('|')]
+        before[params[1]].add(params[0])
+        after[params[0]].add(params[1])
+        line = next(get_line)
+
+    updates = [[int(x) for x in line.split(',')] for line in get_line]
+
+    sum_middle_pages = 0
+    for update in updates:
+        valid = True
+        for i, current_page in enumerate(update):
+            pgs_before = set(update[:i])
+            pgs_after = set(update[i+1:])
+            if not (pgs_before.issubset(before[current_page]) and pgs_after.issubset(after[current_page])):
+                valid = False
+                break
+        if valid:
+            sum_middle_pages += update[len(update)//2]
+
+    print(f'Sum of middle phages: {sum_middle_pages}')
+
+if __name__ == '__main__':
+    main()
+
+#Answer = 4578
---
+++ AdventOfCode/2024/Day-05/puzzle-p2.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+
+DAY = '05'
+
+
+def file_line_gen(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            yield line.strip()
+
+
+def main():
+    filename = fr'./AdventOfCode/2024/Day-{DAY}/input-example.txt'
+    filename = fr'./AdventOfCode/2024/Day-{DAY}/input.txt'
+    
+    before = defaultdict(set)
+    after = defaultdict(set)
+
+    get_line = file_line_gen(filename) 
+    line = next(get_line)
+    while line != '':
+        params = [int(x) for x in line.split('|')]
+        before[params[1]].add(params[0])
+        after[params[0]].add(params[1])
+        line = next(get_line)
+
+    updates = [[int(x) for x in line.split(',')] for line in get_line]
+
+    sum_middle_pages = 0
+    for update in updates:
+        valid = True
+        for i, current_page in enumerate(update):
+            pgs_before = set(update[:i])
+            pgs_after = set(update[i+1:])
+            if not (pgs_before.issubset(before[current_page]) and pgs_after.issubset(after[current_page])):
+                valid = False
+                break
+        if not valid:
+            #Find the page in the middle
+            for page in update:
+                remaining_pages = set(update) - set([page])
+                if len(before[page].intersection(remaining_pages)) == len(remaining_pages)/2:
+                    sum_middle_pages += page
+                    break
+
+    print(f'Sum of middle phages: {sum_middle_pages}')
+
+if __name__ == '__main__':
+    main()
+
+#Answer = 6179
